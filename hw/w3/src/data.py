e9b471ea7b2f3d1bbb2112ebf347c74010f0dbce
dpasupu <dpasupu@ncsu.edu>
2024-01-22 22:06:48 -0500

    like & likes in row

--- hw/w3/src/data.py
+++ hw/w3/src/data.py
@@ -1,7 +1,6 @@
 from cols import COLS
 from rows import ROW
 from utils import *
-import math
 
 class DATA:
     def __init__(self, src, fun=None): 
@@ -40,32 +39,3 @@ class DATA:
             cur_col = self.cols.all[col]
             u[cur_col.txt] = round(getattr(cur_col, fun or "mid")(), nDivs) if nDivs else getattr(cur_col, fun or "mid")()
         return u
-    
-    def like(self, data, n, nHypotheses):
-        prior = (len(data.rows) + the.k) / (n + the.k * nHypotheses)
-        out = math.log(prior)
-        
-        for col in data.cols.x:
-            v = self.cells[col.at]
-            if v != "?":
-                inc = col.like(v, prior)
-                out += math.log(inc)
-        
-        return math.exp(1) ** out
-
-    def likes(self, datas):
-        n, nHypotheses = 0, 0
-
-        for k, data in enumerate(datas):
-            n += len(data.rows)
-            nHypotheses = 1 + nHypotheses
-
-        most, out = None, None
-
-        for k, data in enumerate(datas):
-            tmp = self.like(data, n, nHypotheses)
-
-            if most is None or tmp > most:
-                most, out = tmp, k
-
-        return out, most
--- hw/w3/src/rows.py
+++ hw/w3/src/rows.py
@@ -1,3 +1,34 @@
+from utils import *
+import math
+
 class ROW:
     def __init__(self, cells):
         self.cells = cells
+
+    def like(self, data, n, nHypotheses):
+        prior = (len(data.rows) + the.k) / (n + the.k * nHypotheses)
+        out = math.log(prior)
+        
+        for col in data.cols.x:
+            v = self.cells[col.at]
+            if v != "?":
+                inc = col.like(v, prior)
+                out += math.log(inc)
+        
+        return math.exp(1) ** out
+
+    def likes(self, datas):
+        n, nHypotheses = 0, 0
+
+        for k, data in enumerate(datas):
+            n += len(data.rows)
+            nHypotheses = 1 + nHypotheses
+
+        most, out = None, None
+
+        for k, data in enumerate(datas):
+            tmp = self.like(data, n, nHypotheses)
+            if most is None or tmp > most:
+                most, out = tmp, k
+                
+        return out, most
